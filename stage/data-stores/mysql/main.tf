0573d61aa0366a4b9523687178109ec16ef4d288
Ujstor <astipan@gmail.com>
2024-06-07 01:27:16 +0200

    multi-acc2

--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -1,6 +1,10 @@
 module "mysql_db_stage" {
   source = "../../../modules/data-stores/mysql"
 
+  providers = {
+    aws = aws.stage
+  }
+
   identifier_prefix = "stage-mysql"
   instance_class    = "db.t3.micro"
   db_name           = "stageDB"
